902bd2faaf552128d10318fa9fdf502c7f60bca9
tanipa1 <tania35.291@gmail.com>
2023-03-30 12:01:55 +0600

    Card UI

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
@@ -6,14 +7,19 @@ const Shop = () => {
 
     useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
 
     return (
         <div className='shop-container'>
             <div className='product-container'>
-                <h3>Products are coming: {products.length}</h3>
+                {
+                    products.map(product => <Product 
+                    key={product.id}
+                    product = {product}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>Order Summary</h4>
